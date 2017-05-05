f801cfeff98278f1f3ffc7fc96caba0e15e845c2
Vijaya lakshmi Boyina <vijaya@Vijayas-MacBook-Pro.local>
2017-05-05 12:48:22 -0400

    initial commit

---
+++ src/app/Hero.js
@@ -0,0 +1,10 @@
+"use strict";
+var Hero = (function () {
+    function Hero() {
+        this.id = number;
+        this.name = string;
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=Hero.js.map
